9b10fa08170abb1b943cf44794f2262f763d05c8
Lanzoide <ary.maggiorelli@gmail.com>
2022-01-30 21:10:29 +0100

    BarcheInserimentoTabellaFattodalLotti

--- js/work.js
+++ js/work.js
@@ -3,69 +3,137 @@ const width = 10
 const userSquares = []
 
 function createBoard(grid, squares) {
+    let x = 0, y = 0
     for (let i = 0; i < width*width; i++) {
         const square = document.createElement('div')
         square.style.backgroundColor = "aqua"
         square.setAttribute("id", i)
         square.setAttribute("onclick","it(" + i + ")")
         square.setAttribute("class", "classi")
+        square.setAttribute("data-x", x)
+        square.setAttribute("data-y", y)
         grid.appendChild(square)
         squares.push(square)
+        if (++x == width) {
+            x = 0
+            y++
+        }
     }
 }
 createBoard(userGrid, userSquares);
 
-var recordPoint = []
+
+const barche = [
+    document.getElementById("barca-1"),
+    document.getElementById("barca-2"),
+    document.getElementById("barca-3"),
+    document.getElementById("barca-4"),
+    document.getElementById("barca-5")
+]
+
+var firstBox = null
+var lastBox = null
+var axis
+let counterBox = 1
+var shipIndex = 0
 
 function it(e) {
-    document.getElementById(e).style.backgroundColor = "red";
-    for (let i = 0; i < width*width; i++) {
-        if (recordPoint[i] == null) {
-            recordPoint[i] == e;
+    if (!pippo) {
+        const shipColor = barche[shipIndex].firstChild.style.backgroundColor;
+        let element = document.getElementById(e)
+
+        if (element.getAttribute("data-placed"))
+        {
+            alert("non va bene")
+            return
+        }
+
+        element.setAttribute("data-placed", true)
+        let coordinates = {
+            x: parseInt(element.getAttribute("data-x")),
+            y: parseInt(element.getAttribute("data-y"))
+        }
+        if (firstBox == null) {
+            element.style.backgroundColor = shipColor;
+            firstBox = coordinates
+        } else if(lastBox == null) {
+            if (
+                Math.abs(firstBox.x - coordinates.x) == 1 && firstBox.y == coordinates.y  ||
+                Math.abs(firstBox.y - coordinates.y) == 1 && firstBox.x == coordinates.x 
+            ) {
+                element.style.backgroundColor = shipColor;
+                lastBox = coordinates
+                if (firstBox.x > coordinates.x || firstBox.y > coordinates.y) {
+                    let t = firstBox
+                    firstBox = lastBox
+                    lastBox = t
+                }
+                axis = firstBox.x == coordinates.x ? "y" : "x"
+            } else {
+                alert("non va bene")
+                return;
+            } 
+        } else if (firstBox[axis] - coordinates[axis] == 1) {
+            element.style.backgroundColor = shipColor;
+            firstBox = coordinates
+        } else if (coordinates[axis] - lastBox[axis] == 1) {
+            element.style.backgroundColor = shipColor;
+            lastBox = coordinates
+        } else {
+            alert("non va bene")
+            return
+        }
+
+        if (counterBox++ % barche[shipIndex].childElementCount == 0) {
+            firstBox = lastBox = null
+            axis = null
+            counterBox = 1
+            shipIndex++;
+        }
+
+        if (shipIndex == barche.length)
+        {
+            pippo = true;
         }
     }
 }
 
-const barca1 = document.getElementById("barca-1")
-const barca2 = document.getElementById("barca-2")
-const barca3 = document.getElementById("barca-3")
-const barca4 = document.getElementById("barca-4")
-const barca5 = document.getElementById("barca-5")
-
 var ciao = true;
+var pippo = true;
 
 function ship() {
-    if (ciao == true) {
+    if (ciao) {
         for (let i = 0; i < 4; i++) {
             const ship1 = document.createElement('div');
             ship1.setAttribute("class", "classi")
             ship1.style.backgroundColor = "red";
-            barca1.appendChild(ship1)
+            barche[0].appendChild(ship1)
         }
         for (let i = 0; i < 3; i++) {
             const ship1 = document.createElement('div');
             ship1.setAttribute("class", "classi")
             ship1.style.backgroundColor = "violet";
-            barca2.appendChild(ship1)
+            barche[1].appendChild(ship1)
         }
         for (let i = 0; i < 2; i++) {
             const ship1 = document.createElement('div');
             ship1.setAttribute("class", "classi")
             ship1.style.backgroundColor = "seagreen";
-            barca3.appendChild(ship1)
+            barche[2].appendChild(ship1)
         }
         for (let i = 0; i < 2; i++) {
             const ship1 = document.createElement('div');
             ship1.setAttribute("class", "classi")
             ship1.style.backgroundColor = "slateblue";
-            barca4.appendChild(ship1)
+            barche[3].appendChild(ship1)
         }
         for (let i = 0; i < 1; i++) {
             const ship1 = document.createElement('div');
             ship1.setAttribute("class", "classi")
             ship1.style.backgroundColor = "darkorange";
-            barca5.appendChild(ship1)
+            barche[4].appendChild(ship1)
         }
         ciao = false
+        pippo = false
     }
 }
